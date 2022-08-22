2274a833f2f8913e195754544cbfc3651d32a34f
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-08-22 00:57:25 +0000

    feat: adding working main.tf with my hardcoded email

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
+variable "GCP_ORGANIZATION_ID" {}
+
 locals {
-  org_id                   = ""
+  org_id                   = var.GCP_ORGANIZATION_ID
   company_key              = "venkat"
   gcp_billing_account_name = "My Billing Account"
-  environment                =             "test108"
+  environment              = "test109"
 
   admins = [
     "user:venkata@venkatamutyala.com",
@@ -21,7 +23,7 @@ locals {
   ]
 
 
-  gcp_folder_id = split("/",module.organization_and_project_bootstrap.gcp_folder_id)[1]
+  gcp_folder_id = split("/", module.organization_and_project_bootstrap.gcp_folder_id)[1]
 }
 
 
@@ -39,14 +41,14 @@ module "organization_and_project_bootstrap" {
 
 locals {
   network_prefixes = {
-      kubernetes_pods          = "10.65.0.0/16"
-      gcp_private_connect      = "10.64.128.0/19"
-      kubernetes_services      = "10.64.224.0/20"
-      private_primary          = "10.64.0.0/23"
-      public_primary           = "10.64.64.0/23"
-      serverless_vpc_connector = "10.64.96.0/28"
-      kubernetes_master_nodes  = "10.64.96.16/28"
-    }
+    kubernetes_pods          = "10.65.0.0/16"
+    gcp_private_connect      = "10.64.128.0/19"
+    kubernetes_services      = "10.64.224.0/20"
+    private_primary          = "10.64.0.0/23"
+    public_primary           = "10.64.64.0/23"
+    serverless_vpc_connector = "10.64.96.0/28"
+    kubernetes_master_nodes  = "10.64.96.16/28"
+  }
 }
 
 module "vpc" {
@@ -55,8 +57,8 @@ module "vpc" {
   workspace = local.environment
   region    = "us-central1"
 
-  network_prefixes = local.network_prefixes
-  gcp_folder_id = local.gcp_folder_id
+  network_prefixes                   = local.network_prefixes
+  gcp_folder_id                      = local.gcp_folder_id
   enable_google_vpc_access_connector = false
   depends_on = [
     module.organization_and_project_bootstrap
@@ -64,14 +66,14 @@ module "vpc" {
 }
 
 
-module "gke" {
-  source = "git::https://github.com/GlueOps/terraform-gcp-gke-module.git"
+# module "gke" {
+#   source = "git::https://github.com/GlueOps/terraform-gcp-gke-module.git"
 
-  workspace     = local.environment
-  region        = "us-central1"
-  gcp_folder_id = local.gcp_folder_id
+#   workspace     = local.environment
+#   region        = "us-central1"
+#   gcp_folder_id = local.gcp_folder_id
 
-    depends_on = [
-    module.vpc
-  ]
-}
+#     depends_on = [
+#     module.vpc
+#   ]
+# }
