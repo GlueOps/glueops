ec0a3f2925a3088ccfac5a8a37524724d0c43c31
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-09-21 22:02:12 +0000

    feat: working commit to spin up accounts

--- aws/main.tf
+++ aws/main.tf
@@ -7,23 +7,51 @@ terraform {
   }
 }
 
-data "aws_iam_account_alias" "current" {}
+### Use your OWN aws account credentials here!
+## export TF_VAR_MANAGEMENT_AWS_ACCESS_KEY_ID=""
+## export TF_VAR_MANAGEMENT_AWS_ACCESS_SECRET_KEY=""
+variable "MANAGEMENT_AWS_ACCESS_KEY_ID" {}
+variable "MANAGEMENT_AWS_ACCESS_SECRET_KEY" {}
 
-output "account_alias" {
-  value = data.aws_iam_account_alias.current.id
+provider "aws" {
+    region     = "us-west-2"
+    access_key = var.MANAGEMENT_AWS_ACCESS_KEY_ID
+    secret_key = var.MANAGEMENT_AWS_ACCESS_SECRET_KEY
 }
 
+### use the terraform-svc account 
+## export AWS_ACCESS_KEY_ID=""
+## export AWS_SECRET_ACCESS_KEY=""
+provider "aws" {
+  alias  = "nonprod"
+  assume_role {
+    role_arn     = "arn:aws:iam::041618144804:role/OrganizationAccountAccessRole"
+  }
+}
 
-# resource "aws_organizations_account" "account" {
-#   name  = "rocks_nonprod"
-#   email = "rocks@glueops.dev"
-#   close_on_deletion = true
-#   role_name = module.organization_access_role.role_name
+data "aws_caller_identity" "current" {}
+
+
+# resource "aws_organizations_organization" "org" {
+#   aws_service_access_principals = [
+#     "cloudtrail.amazonaws.com",
+#     "config.amazonaws.com",
+#   ]
+
+#   feature_set = "ALL"
 # }
 
-# module "organization_access_role" {
-#   source            = "git::https://github.com/cloudposse/terraform-aws-organization-access-role.git?ref=master"
-#   master_account_id = "XXXXXXXXXXXX"
-#   role_name         = "OrganizationAccountAccessRole"
-#   policy_arn        = "arn:aws:iam::aws:policy/AdministratorAccess"
-# }
+resource "aws_organizations_account" "account" {
+  name  = "rocks_nonprod"
+  email = "rocks+202209213@glueops.dev" #${formatdate("YYYYMMDD", timestamp())}
+  close_on_deletion = true
+  role_name = module.organization_access_role.role_name
+}
+
+module "organization_access_role" {
+  source            = "git::https://github.com/maxgio92/terraform-aws-organization-access-role.git?ref=feature/012-upgrade"
+  master_account_id = data.aws_caller_identity.current.account_id
+  role_name         = "OrganizationAccountAccessRole"
+  policy_arn        = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
