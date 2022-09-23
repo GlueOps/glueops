00ac93fcfe4531d0e3cd227ffb82c118b47d7fcf
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-09-23 21:03:40 +0000

    feat: rough wip for aws

--- aws/main.tf
+++ aws/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 
-## Everything commented below was pretty much a one-time setup.
+# # Everything commented below was pretty much a one-time setup.
 # variable "MANAGEMENT_AWS_ACCESS_KEY_ID" {}
 # variable "MANAGEMENT_AWS_SECRET_ACCESS_KEY" {}
 # provider "aws" {
@@ -21,8 +21,8 @@ terraform {
 # }
 
 
-## Commented out to prevent redeployment of the organization, and it can't be destroyed due to SUSPENDED accounts.
-## This only needs to be done one time since it converts the aws account into a organization management account
+# ## Commented out to prevent redeployment of the organization, and it can't be destroyed due to SUSPENDED accounts.
+# ## This only needs to be done one time since it converts the aws account into a organization management account
 # resource "aws_organizations_organization" "org" {
 #   aws_service_access_principals = [
 #     "cloudtrail.amazonaws.com",
@@ -32,8 +32,8 @@ terraform {
 #   feature_set = "ALL"
 # }
 
-## This role gets created in the root/management organization account and should be used for all sub accounts that get created. 
-## This role has already been created so you shouldn't need to ever create this rsource again.
+# ## This role gets created in the root/management organization account and should be used for all sub accounts that get created. 
+# ## This role has already been created so you shouldn't need to ever create this rsource again.
 # module "organization_access_role" {
 #   source            = "git::https://github.com/glueops/terraform-aws-organization-access-role.git?ref=master"
 #   providers = {
@@ -45,19 +45,19 @@ terraform {
 # }
 
 
-## Given the limits of account creation in AWS we are just creating an admiral/captain account and plan to use a third party OSS solution to nuke what's in the accounts whenever we want to reset them.
-# resource "aws_organizations_account" "account" {
+# # Given the limits of account creation in AWS we are just creating an admiral/captain account and plan to use a third party OSS solution to nuke what's in the accounts whenever we want to reset them.
+# resource "aws_organizations_account" "account_admiral" {
 #   provider = aws.management_account
-#   name  = "rocks_20220922_admiral"
-#   email = "rocks+20220922-admiral@glueops.dev" #${formatdate("YYYYMMDD", timestamp())}
+#   name  = "fernando_admiral"
+#   email = "rodriguez.fernandoa+aws-admiral@gmail.com" #${formatdate("YYYYMMDD", timestamp())}
 #   close_on_deletion = true
 #   role_name = "OrganizationAccountAccessRole"
 # }
 
-# resource "aws_organizations_account" "account" {
+# resource "aws_organizations_account" "account_captain" {
 #   provider = aws.management_account
-#   name  = "rocks_20220922_captain"
-#   email = "rocks+20220922-captain@glueops.dev" #${formatdate("YYYYMMDD", timestamp())}
+#   name  = "fernando_captain"
+#   email = "rodriguez.fernandoa+aws-captain@gmail.com" #${formatdate("YYYYMMDD", timestamp())}
 #   close_on_deletion = true
 #   role_name = "OrganizationAccountAccessRole"
 # }
@@ -68,97 +68,220 @@ terraform {
 
 
 
-
-
+# locals {
+#   admiral_arn = "arn:aws:iam::394776142380:role/OrganizationAccountAccessRole"
+#   captain_arn = "arn:aws:iam::816624901837:role/OrganizationAccountAccessRole"
+# }
 
 provider "aws" {
+  alias = "admiral"
   assume_role {
-    role_arn     = "arn:aws:iam::041618144804:role/OrganizationAccountAccessRole"
+    role_arn     = "arn:aws:iam::539362929792:role/OrganizationAccountAccessRole"
   }
 }
 
+provider "aws" {
+  alias = "captain"
+  assume_role {
+    role_arn     = "arn:aws:iam::723876146658:role/OrganizationAccountAccessRole"
+  }
+}
+
+#read ak sak st < <(echo $(aws sts assume-role --role-arn "arn:aws:iam::723876146658:role/OrganizationAccountAccessRole" --role-session-name k8s-test | jq -r '.Credentials | "\(.AccessKeyId) \(.SecretAccessKey) \(.SessionToken)"')) && export AWS_ACCESS_KEY_ID=$ak AWS_SECRET_ACCESS_KEY=$sak AWS_SESSION_TOKEN=$st
 
 
-# #=== CloudPosse EKS
+#=== CloudPosse EKS
 
 
-#   module "label" {
-#     source = "cloudposse/label/null"
-#     # Cloud Posse recommends pinning every module to a specific version
-#     # version  = "x.x.x"
+  module "label" {
+    source = "cloudposse/label/null"
+    # Cloud Posse recommends pinning every module to a specific version
+    # version  = "x.x.x"
 
-#     namespace  = "test-nonprod"
-#     name       = "test-name"
-#     stage      = "test-stage"
-#     delimiter  = "-"
-#     attributes = ["cluster"]
-#     tags       = {"tag": "test-tag"}
-#   }
+    namespace  = "test-nonprod"
+    name       = "test-name"
+    stage      = "test-stage"
+    delimiter  = "-"
+    attributes = ["cluster"]
+    tags       = {"tag": "test-tag"}
+  }
 
-#   locals {
-#     # Prior to Kubernetes 1.19, the usage of the specific kubernetes.io/cluster/* resource tags below are required
-#     # for EKS and Kubernetes to discover and manage networking resources
-#     # https://www.terraform.io/docs/providers/aws/guides/eks-getting-started.html#base-vpc-networking
-#     tags = { "kubernetes.io/cluster/${module.label.id}" = "shared" }
-#   }
+  locals {
+    # Prior to Kubernetes 1.19, the usage of the specific kubernetes.io/cluster/* resource tags below are required
+    # for EKS and Kubernetes to discover and manage networking resources
+    # https://www.terraform.io/docs/providers/aws/guides/eks-getting-started.html#base-vpc-networking
+    tags = { "kubernetes.io/cluster/${module.label.id}" = "shared" }
+  }
 
-#   module "vpc" {
-#     source = "cloudposse/vpc/aws"
-#     # Cloud Posse recommends pinning every module to a specific version
-#     # version     = "x.x.x"
-#     cidr_block = "10.65.0.0/16"
 
-#     tags    = local.tags
-#     context = module.label.context
-#   }
+locals {
+  vpc = {
+    cidr_block = "10.65.0.0/16"
+  }
+}
 
-#   module "subnets" {
-#     source = "cloudposse/dynamic-subnets/aws"
-#     # Cloud Posse recommends pinning every module to a specific version
-#     # version     = "x.x.x"
+  module "vpc_admiral" {
+    providers = {
+      aws = aws.admiral
+    }
+    source = "cloudposse/vpc/aws"
+    # Cloud Posse recommends pinning every module to a specific version
+    # version     = "x.x.x"
+    cidr_block = local.vpc.cidr_block
 
-#     vpc_id               = module.vpc.vpc_id
-#     igw_id               = [module.vpc.igw_id]
-#     nat_gateway_enabled  = true
-#     nat_instance_enabled = false
+    tags    = local.tags
+    context = module.label.context
+  }
 
-#     tags    = local.tags
-#     context = module.label.context
-#     availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
-#   }
+   module "vpc_captain" {
+    providers = {
+      aws = aws.captain
+    }
+    source = "cloudposse/vpc/aws"
+    # Cloud Posse recommends pinning every module to a specific version
+    # version     = "x.x.x"
+    cidr_block = local.vpc.cidr_block
 
-#   module "eks_node_group" {
-#     source = "cloudposse/eks-node-group/aws"
-#     # Cloud Posse recommends pinning every module to a specific version
-#     # version     = "x.x.x"
+    tags    = local.tags
+    context = module.label.context
+  }
 
-#     instance_types                     = ["t3a.xlarge"]
-#     subnet_ids                         = module.subnets.public_subnet_ids
-#     #health_check_type                  = var.health_check_type
-#     desired_size                       = 3
-#     min_size                           = 3
-#     max_size                           = 4
-#     cluster_name                       = module.eks_cluster.eks_cluster_id
+  module "subnets_admiral" {
+    providers = {
+      aws = aws.admiral
+    }
+    source = "cloudposse/dynamic-subnets/aws"
+    # Cloud Posse recommends pinning every module to a specific version
+    # version     = "x.x.x"
 
-#     # Enable the Kubernetes cluster auto-scaler to find the auto-scaling group
-#     cluster_autoscaler_enabled = true
+    vpc_id               = module.vpc_admiral.vpc_id
+    igw_id               = [module.vpc_admiral.igw_id]
+    nat_gateway_enabled  = true
+    nat_instance_enabled = false
 
-#     context = module.label.context
+    tags    = local.tags
+    context = module.label.context
+    availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  }
 
-#     # Ensure the cluster is fully created before trying to add the node group
-#     module_depends_on = module.eks_cluster.kubernetes_config_map_id
-#   }
+    module "subnets_captain" {
+    providers = {
+      aws = aws.captain
+    }
+      source = "cloudposse/dynamic-subnets/aws"
+      # Cloud Posse recommends pinning every module to a specific version
+      # version     = "x.x.x"
 
-#   module "eks_cluster" {
-#     source = "cloudposse/eks-cluster/aws"
-#     # Cloud Posse recommends pinning every module to a specific version
-#     # version = "x.x.x"
-#     region = "us-west-2"
-#     vpc_id     = module.vpc.vpc_id
-#     subnet_ids = module.subnets.public_subnet_ids
+      vpc_id               = module.vpc_captain.vpc_id
+      igw_id               = [module.vpc_captain.igw_id]
+      nat_gateway_enabled  = true
+      nat_instance_enabled = false
 
-#     oidc_provider_enabled = true
+      tags    = local.tags
+      context = module.label.context
+      availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  }
+  
 
-#     context = module.label.context
-#     kubernetes_version = "1.22"
-#   }
+
+  locals {
+    eks_node_group = {
+      instance_types = ["t3a.medium"]
+    }
+  }
+
+  module "eks_node_group_admiral" {
+    providers = {
+      aws = aws.admiral
+    }
+    source = "cloudposse/eks-node-group/aws"
+    # Cloud Posse recommends pinning every module to a specific version
+    # version     = "x.x.x"
+
+    instance_types                     = local.eks_node_group.instance_types
+    subnet_ids                         = module.subnets_admiral.public_subnet_ids
+    #health_check_type                  = var.health_check_type
+    desired_size                       = 3
+    min_size                           = 3
+    max_size                           = 4
+    cluster_name                       = module.eks_cluster_admiral.eks_cluster_id
+
+    # Enable the Kubernetes cluster auto-scaler to find the auto-scaling group
+    cluster_autoscaler_enabled = true
+
+    context = module.label.context
+
+    # Ensure the cluster is fully created before trying to add the node group
+    module_depends_on = module.eks_cluster_admiral.kubernetes_config_map_id
+  }
+
+
+
+  module "eks_node_group_captain" {
+    providers = {
+      aws = aws.captain
+    }
+    source = "cloudposse/eks-node-group/aws"
+    # Cloud Posse recommends pinning every module to a specific version
+    # version     = "x.x.x"
+
+    instance_types                     = local.eks_node_group.instance_types
+    subnet_ids                         = module.subnets_captain.public_subnet_ids
+    #health_check_type                  = var.health_check_type
+    desired_size                       = 3
+    min_size                           = 3
+    max_size                           = 4
+    cluster_name                       = module.eks_cluster_captain.eks_cluster_id
+
+    # Enable the Kubernetes cluster auto-scaler to find the auto-scaling group
+    cluster_autoscaler_enabled = true
+
+    context = module.label.context
+
+    # Ensure the cluster is fully created before trying to add the node group
+    module_depends_on = module.eks_cluster_captain.kubernetes_config_map_id
+  }
+
+
+  locals {
+    eks_cluster = {
+      cluster_version = "1.22"
+      region = "us-west-2"
+    }
+  }
+
+  module "eks_cluster_admiral" {
+    providers = {
+      aws = aws.admiral
+    }
+    source = "cloudposse/eks-cluster/aws"
+    # Cloud Posse recommends pinning every module to a specific version
+    # version = "x.x.x"
+    region = local.eks_cluster.region
+    vpc_id     = module.vpc_admiral.vpc_id
+    subnet_ids = module.subnets_admiral.public_subnet_ids
+
+    oidc_provider_enabled = true
+
+    context = module.label.context
+    kubernetes_version = local.eks_cluster.cluster_version
+  }
+
+
+
+  module "eks_cluster_captain" {
+    providers = {
+      aws = aws.captain
+    }
+    source = "cloudposse/eks-cluster/aws"
+    # Cloud Posse recommends pinning every module to a specific version
+    # version = "x.x.x"
+    region = local.eks_cluster.region
+    vpc_id     = module.vpc_captain.vpc_id
+    subnet_ids = module.subnets_captain.public_subnet_ids
+
+    oidc_provider_enabled = true
+
+    context = module.label.context
+    kubernetes_version = local.eks_cluster.cluster_version
+  }
