2b07c16a4608fdb142c42abbcb020a40a4063fdc
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-11-14 01:17:05 +0000

    feat: added gha.sh

---
+++ gha.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+task cluster_up
+task configs
+task bootstrap_admiral
+task shared:bootstrap_captain
+sleep 1800 # wait for captain to be ready
+task shared:send_credentials_to_slack
+sleep 1800 # Give you an opportunity to look at everything.
+task clean
