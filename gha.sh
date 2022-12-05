1408c6374b26650577b466dff1670a79ffc213c3
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-12-05 11:07:18 -0800

    feat: increase sleep time for gha.sh

--- gha.sh
+++ gha.sh
@@ -6,5 +6,5 @@ task bootstrap_admiral
 task shared:bootstrap_captain
 sleep 1800 # wait for captain to be ready
 task shared:send_credentials_to_slack
-sleep 1800 # Give you an opportunity to look at everything.
+sleep 3600 # Give you an opportunity to look at everything.
 task clean
